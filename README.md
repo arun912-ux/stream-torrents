a6457532a47086f96fd56e69ae897f6fd3d5297d
arun912-ux <arunshanagonda777@gmail.com>
2022-05-13 19:10:51 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,47 @@
+# Play and Stream torrent files in VLC - Windows 
+
+### Shotout
+This project is inspired from Bugs Writer [notflix](https://github.com/bugswriter/notflix).  
+He used bash scripting, so it only run on Linux based system. 
+
+So this project is totally wriiten for Windows users in Python Language
+
+
+
+### How does this work?
+
+This is a python script. It scape 1337x and get the magnet link.
+After this it use [peerflix](https://github.com/mafintosh/peerflix) to stream the video from magnet link.
+For scraping script use simple gnu utils like sed, awk, paste, cut.
+
+
+## Dependencies
+
+1. node.js
+1. peerflix
+1. VLC
+
+
+### to install dependencies:
+
+1.  [Click](https://nodejs.org/dist/v16.15.0/node-v16.15.0-x64.msi) to download NodeJS installer
+
+1.  Install NodeJS by executing the .msi file
+
+1.  Click Start and open NodeJS Command Prompt and type<p>
+    ```npm install -g peerflix ```
+
+1.  [Click](https://mirrors.estointernet.in/videolan/vlc/3.0.3/win64/vlc-3.0.3-win64.exe) to download VLC Player and install it.
+
+
+## How to run
+
+- clone or download this repo. 
+- open Terminal or PowerShell or Command Prompt in the repo folder.
+- to run the script,  and just type <p>
+``` search-final.py  <moviename>```
+
+- select the file from drop down. 
+> Hint : Select the top ones since they have more seeders.
+
+- That's it. Your searched file will be play in the VLC player.
---
+++ search-final.py
@@ -0,0 +1,103 @@
+import sys
+import http.client
+import re
+from types import NoneType
+import subprocess, sys
+from time import sleep
+
+
+#print(len(sys.argv))
+s=""
+for i in range(1, len(sys.argv)):
+    #print(sys.argv[i])
+    s = s + sys.argv[i] + "+"
+
+
+# print(s)
+query = s
+# ct.function(s)
+
+conn = http.client.HTTPSConnection("1337x.wtf")
+
+headers = {
+    'accept-charset': "utf-8",
+    'cache-control': "no-cache"
+    }
+
+
+sear = "/search/" + query + "/1/"
+conn.request("GET", sear , headers=headers)
+
+res = conn.getresponse()
+data = res.read()
+resp = data.decode('utf-8')
+
+fi = open('rep.txt', 'w', encoding = 'utf-8')
+fi.write(resp)
+fi.close()
+
+fi = open('rep.txt', 'r', encoding = 'utf-8')
+
+# m = re.search("torrent\/[0-9]{7}\/[a-zA-Z0-9?%-]*/", fi.readlines())
+
+# for i in range(0, len(m.group())):
+#     print(m.group(1))
+i=1
+lis=[]
+for line in fi.readlines():
+    # print(line)
+    k = line.find('torrent/')
+    if(k != -1):
+        # print(line)cl
+        # n = line.index("torrent\/[0-9]{7}\/[a-zA-Z0-9?%-]*/")
+        # print()
+        m = re.search("torrent\/[0-9]{7}\/[a-zA-Z0-9?%-]*/", line)
+        if (type(m) != NoneType):
+            print(str(i) + " " + m.group(0))
+            i+=1
+            lis.append(m.group(0))
+        # break
+    # print(k)
+
+fi.close()
+
+n = int(input("Enter number : "))
+torrent = lis[n-1]
+
+conn = http.client.HTTPSConnection("1337x.wtf")
+
+headers = {
+    'accept-charset': "utf-8",
+    'cache-control': "no-cache"
+    }
+
+
+conn.request("GET", "/"+torrent, headers=headers)
+
+
+res = conn.getresponse()
+data = res.read()
+
+# print(data.decode("utf-8"))
+resp = data.decode('utf-8')
+m = re.search("magnet:\?xt=urn:btih:[a-zA-Z0-9&=%+\.-]*", resp)
+# print(m.group(0))
+
+
+
+magnet = m.group(0)
+
+# print(magnet)
+
+runcmd = "peerflix.cmd \"" + magnet + "\" --vlc"
+print(runcmd)
+
+# ps = open('file.ps1', 'w', encoding='utf-8')
+# ps.write(runcmd)
+# ps.close()
+
+sleep(2)
+
+p = subprocess.Popen(runcmd, stdout=sys.stdout)
+
+p.communicate()

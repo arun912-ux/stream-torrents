70b84847fb13656a6fdbe2ef883d5820965bb52a
arun912-ux <arunshanagonda777@gmail.com>
2022-05-19 12:09:53 +0530

    changes in dependenies.bat

--- install_dependecies.bat
+++ install_dependecies.bat
@@ -6,7 +6,7 @@
 @REM install chocolety
 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command "[System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
-@REM  install quietly
+@REM  install nodejs
 choco install nodejs -y
 
 @REM install vlc
@@ -17,8 +17,3 @@ npm install -g peerflix
 
 @REM install qbittorrent
 choco install qbittorrent -y
-
-
-
-
-
